79106f68e51d0307e8c5b3afcef53818fe754aac
Gabriel Diniz <55710093+GabeDiniz@users.noreply.github.com>
2024-01-16 15:16:56 -0500

    Update: top-k-frequent-elements

--- Neetcode/1 Arrays and Hashing/top-k-frequent-elements.py
+++ Neetcode/1 Arrays and Hashing/top-k-frequent-elements.py
@@ -13,3 +13,23 @@ Example 2:
 Input: nums = [1], k = 1
 Output: [1]
 '''
+
+# Time Complexity: Worse case -> O(2n) which belongs to O(n) 
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        hashMap = {}
+        result = []
+        for num in nums:
+            # If the number exists in the Hashmap, add 1
+            if num in hashMap:
+                hashMap[num] +=1
+            # If the number does no exist, set it to 1
+            else:
+                hashMap[num] = 1
+        # For each number that appeared above, check if the value
+        #   is >= k (i.e., the number appeared a minimum of k times)
+        #   If it did -> append it to our result. 
+        for key in hashMap.keys():
+            if hashMap[key] >= k:
+                result.append(key)
+        return result
