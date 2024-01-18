774b256f911a289f8db5b5a66f1a065c7e34b8f7
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-01-18 13:56:34 -0500

    Add: product-of-array solution

--- Neetcode/1 Arrays and Hashing/product-of-array-except-self.py
+++ Neetcode/1 Arrays and Hashing/product-of-array-except-self.py
@@ -18,3 +18,33 @@ Example 2:
 Input: nums = [-1,1,0,-3,3]
 Output: [0,0,9,0,0]
 '''
+
+# Time Complexity: O(n)
+# O(1) Space Complexity 
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        answer = [0] * (len(nums))
+        
+        # Prefix
+        # Calculate the prefix product as you move right to left
+        #   Example: prefix product of [2, 4, 5, 10] -> [1, 2, 8, 40]
+        prefix = 1
+        for i in range(len(nums)):    
+            answer[i] = prefix
+            prefix *= nums[i]
+
+        # Suffix
+        # Calculate the suffix product as you move right to left
+        #   and multiply it by the respective suffix to determine
+        #   the product of array except self
+        #   Example: suffix product of [2, 4, 5, 10] -> [200, 50, 10, 1]
+        #       Instead of doing an intermediate step, we can multiply that
+        #       by the suffix to get the answer for each position
+        #   Example: With the prefix product being - [1, 2, 8, 40]
+        #       the suffix calculates 40*suffix (40 * 1) ->
+        #       8*suffix (8 * 10) -> 2*suffix (2 * 50) -> 1*suffix (1 * 200)
+        suffix = 1
+        for n in range(len(nums) - 1, -1, -1):
+            answer[n] *= suffix
+            suffix *= nums[n]
+        return answer
