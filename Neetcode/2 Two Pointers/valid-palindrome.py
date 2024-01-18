5824907b1c3b0025e805d7a2363cf01e6a94ff24
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-01-18 16:26:27 -0500

    Create: valid-palindrome

---
+++ Neetcode/2 Two Pointers/valid-palindrome.py
@@ -0,0 +1,38 @@
+'''
+Valid Palindrome
+
+Question: A phrase is a palindrome if, after converting all uppercase 
+  letters into lowercase letters and removing all non-alphanumeric characters, 
+  it reads the same forward and backward. Alphanumeric characters include 
+  letters and numbers.
+
+  Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+  Since an empty string reads the same forward and backward, it is a palindrome.
+Output:
+'''
+
+# Time Complexity: O(n)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Format the string
+        s = s.replace(" ", "").lower()
+        s = ''.join(letter for letter in s if letter.isalnum())
+        # Check if its the same (forwards == backwards)
+        if s == s[::-1]: 
+            return True
+        return False
