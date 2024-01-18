a0d5ebafc4aa10921da7858b6e869ee8e986ffbb
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-01-18 15:45:42 -0500

    Update: add solution 2

--- Leetcode/contains-duplicate-2.py
+++ Leetcode/contains-duplicate-2.py
@@ -19,11 +19,38 @@ Input: nums = [1,2,3,1,2,3], k = 2
 Output: false
 '''
 
+from collections import defaultdict
+
 class Solution:
     def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+        
+        # Solution 2: 
+        # Time Complexity: O(n)
+        hashMap = defaultdict(int)
+        window = nums[0:k + 1]
+        # Create a Hashmap of the initial window based on k
+        for num in window:
+            hashMap[num] += 1
+            if hashMap[num] > 1:
+                return True
+
+        # Update the left of the window (the number to the left that
+        #   is no longer in the window) and update the Hashmap to reflect
+        #   the new number that has entered the window. 
+        left = 0
+        for num in nums[k + 1:]:
+            hashMap[nums[left]] -= 1
+            if hashMap[num] == 0:
+                hashMap[num] += 1
+            elif hashMap[num] == 1:
+                return True
+            left += 1
+        
+        # Solution 1:
         # Time Complexity: O(n*k)        
         for i, num in enumerate(nums):
             sublist = nums[i + 1:i + k + 1]
             if num in sublist:
                 return True
         return False
+    
