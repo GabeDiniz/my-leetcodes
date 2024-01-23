a59a863f4b7b83358d61acce421c1714fa6dbe9c
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-01-23 16:27:16 -0500

    Create: container-with-most-water solution

---
+++ Neetcode/2 Two Pointers/container-with-most-water.py
@@ -0,0 +1,46 @@
+'''
+Container with Most Water
+
+You are given an integer array height of length n. There are n 
+  vertical lines drawn such that the two endpoints of the ith 
+  line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, 
+  such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+  Notice that you may not slant the container.
+
+Example 1:
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. 
+  In this case, the max area of water the container can contain is 49.
+  That's because the max area is generated from 8 (left) and 7 (right). 
+
+  The container can't hold above 7 since it will overflow. Thus, the height is 7.
+  The width is also 7 since the left (8) is in position 1 and right (7) is in position 8.
+  To get the width, you can do 8 - 1 (or left-index - right-index). 
+
+Example 2:
+Input: height = [1,1]
+Output: 1
+'''
+
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        
+
+        # Solution 1: O(n^2) - TOO SLOW
+        max = 0
+        # Left vertical line
+        for l in range(len(height)):
+            # Right vertical line
+            for r in range(l + 1, len(height)):
+                left = height[l]    # left number
+                right = height[r]   # right number
+                width = r - l       # width (right-index - left-index)
+                area = min(left, right) * width
+                if area > max:
+                    max = area
+        return max
