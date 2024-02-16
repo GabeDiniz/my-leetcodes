1b4343741a9f33b95e0dd4a0fffe67ebea85aa5a
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-02-16 16:25:59 -0500

    Update least-number-of-unique-integers.py

--- Leetcode/least-number-of-unique-integers.py
+++ Leetcode/least-number-of-unique-integers.py
@@ -14,3 +14,21 @@ Input: arr = [4,3,1,1,3,3,2], k = 3
 Output: 2
 Explanation: Remove 4, 2 and either one of the two 1s or three 3s. 1 and 3 will be left.
 '''
+
+from collections import Counter
+
+class Solution:
+  def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+    # Map the frequency
+    frequency = Counter(arr)
+    # Sort the frequency as a num-frequency set (i.e., [(3, 2), (2, 4), ...] )
+    sorted_frequency = sorted(frequency.items(), key = lambda x: x[1])
+    
+    # Go through the sorted num-frequency pair, removing the least frequent
+    #   until removing k from frequency does not remove the number
+    for num, freq in sorted_frequency:
+      if k >= freq:
+        k -= freq # subtract that numbers frequency
+        del frequency[num] # remove the number from the frequency_map
+      else: break
+    return len(frequency)
