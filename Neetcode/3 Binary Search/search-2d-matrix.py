3ccf715921400d020a8e04aa2f95a914da3e5f11
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-02-20 17:02:00 -0500

    Add: search-2d-matrix solution

--- Neetcode/3 Binary Search/search-2d-matrix.py
+++ Neetcode/3 Binary Search/search-2d-matrix.py
@@ -23,3 +23,39 @@ n == matrix[i].length
 1 <= m, n <= 100
 -104 <= matrix[i][j], target <= 104
 '''
+
+class Solution:
+  def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+    # Time Complexity: O(log(m * n))
+    top, bottom, row = 0, len(matrix) - 1, []  
+    left, right = 0, len(matrix[0]) - 1 # left column and right column
+
+    # Find row
+    # for i, row in enumerate(matrix):
+    while top <= bottom:
+      middle = (top + bottom) // 2
+      # If the row's left bound is < target and right bound is > target
+      #   we have found our row -> break loop
+      if matrix[middle][left] <= target and matrix[middle][right] >= target:
+        row = matrix[middle]
+        break
+      elif matrix[middle][left] > target:
+        bottom = middle - 1
+      elif matrix[middle][right] < target:
+        top = middle + 1
+    # If the row is still its default value (empty), the target is not present
+    if not row: return False
+
+    # Row is now the list that should contain our target
+    # Perform binary search on that row
+    while left <= right:
+      middle = (left + right) // 2
+      # If the middle number is < target -> move left pointer
+      if row[middle] < target:
+        left = middle + 1
+      # If the middle number is > target -> move right pointer
+      elif row[middle] > target:
+        right = middle - 1
+      else: 
+          return True
+    return False
