b8077d7d2f1521789dc529763584b5c1dbc59432
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-04-16 13:51:15 -0400

    [Update] remove-element with solution

--- Top-Interview-150/remove-element.py
+++ Top-Interview-150/remove-element.py
@@ -3,3 +3,16 @@ Remove Element
 
 Link: https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150
 '''
+
+class Solution:
+  def removeElement(self, nums: list[int], val: int) -> int:
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+    pos = 0
+    for num in nums:
+      # Shift nums left if they are not equal to val
+      if num != val:
+        nums[pos] = num
+        pos += 1
+
+    return pos
