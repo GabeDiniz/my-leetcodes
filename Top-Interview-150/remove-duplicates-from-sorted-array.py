79b562679d025cd59610d44c5ebe493416c4a15a
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-04-16 14:12:27 -0400

    Update remove-duplicates-from-sorted-array.py

--- Top-Interview-150/remove-duplicates-from-sorted-array.py
+++ Top-Interview-150/remove-duplicates-from-sorted-array.py
@@ -3,3 +3,19 @@ Remove Duplicates from Sorted Array
 
 Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
 '''
+
+class Solution:
+  def removeDuplicates(self, nums: list[int]) -> int:
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+
+    # Use set to keep track of unique values
+    hashSet = set()
+    index = 0
+    for num in nums:
+      if num not in hashSet:
+        nums[index] = num
+        hashSet.add(num)
+        index += 1
+    # Return the length of unique values
+    return len(hashSet)
